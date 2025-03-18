0c5b68f594cd069952e4d835b8b2c09e5ef7a5a3
rafaeladurand <rafaeladurandcontato@gmail.com>
2025-03-17 23:58:41 -0300

    chore: add comments

--- trabalho_fase1/database/db.js
+++ trabalho_fase1/database/db.js
@@ -6,7 +6,9 @@ const connectDatabase = () => {
   mongoose
     .connect(
       "mongodb+srv://root:root@fase1.0fpkd.mongodb.net/?retryWrites=true&w=majority&appName=Fase1",
-      { useNewUrlParser: true, useUnifiedTopology: true }
+      { useNewUrlParser: true, useUnifiedTopology: true } 
+      // Usa o novo parser de URL do MongoDB
+      // Habilita o novo mecanismo de gerenciamento de conexões
     )
     .then(() => console.log("MongoDB Atlas Connected"))
     .catch((error) => console.log(error));
